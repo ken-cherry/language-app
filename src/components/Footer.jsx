8683ab1de5b4042d30414115227101797316d7b0
ken-cherry <ken@cherrywebdev.com>
2024-02-24 16:41:38 -0600

    Styled: footer and next/prev buttons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer-btn">
-      <button className="btn">
+      <button className="btn home-btn">
         <Link to="/">Home</Link>
       </button>
     </div>
