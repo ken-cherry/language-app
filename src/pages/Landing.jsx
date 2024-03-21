3fa9438bf62ae356482073751c94f8b175c91c94
ken-cherry <ken@cherrywebdev.com>
2024-03-21 10:21:05 -0500

    Added: h5 to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,6 +5,7 @@ const Landing = () => {
   return (
     <>
       <div className="landing-page">
+        <h5>Select the flag of the language you want to practice.</h5>
         <img
           src="../images/charlieBoat.jpg"
           alt="Image of Charlie"
